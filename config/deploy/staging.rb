80122ac3b4c196e105bbf496fec07041bd8c8180
essential data <devs@essential-data.sk>
2014-07-31 15:38:46 +0200

    Chang staging deploy branch to develop

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,7 +1,7 @@
 server ENV['OPENREPLY_DEPLOY_SERVER_STAGING'], :app, :web, :db, :primary => true
 
 set :deploy_to, ENV['OPENREPLY_DEPLOY_TO_STAGING']
-set :branch, "feature/refactoring"
+set :branch, "develop"
 set :rails_env, "staging"
 
 set :rvm_type, ENV['OPENREPLY_DEPLOY_RVM_TYPE_STAGING'].to_sym # Default rvm_type is :user
