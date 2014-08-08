112b3e72b877d87355dc4bc9155487debc73a096
lubosch <lubomir.vnenk@zoho.com>
2014-08-08 10:03:19 +0200

    settings change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,10 +66,11 @@ Openreply::Application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # action_mailer settings
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       address: ENV["SMTP_ADDRESS"],
-      port: 587,
-      domain: ENV['SMTP_DOMAIN'],
+      port: ENV["SMTP_PORT"],
+      domain: ENV["SMTP_DOMAIN"],
       authentication: "plain",
       user_name: ENV["SMTP_USERNAME"],
       password: ENV["SMTP_PASSWORD"]
