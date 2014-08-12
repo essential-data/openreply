a6b72ace40b296872ce60026857431e77a33c975
lubosch <lubomir.vnenk@zoho.com>
2014-08-12 12:53:58 +0200

    specs refactor

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,7 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Review, :type => :model do
+  describe '#reviewed?' do
+    it 'should not be reviewed' do
+      r= Review.new
+      expect(r.reviewed?).to be false
+    end
 
+    it 'should be reviewed' do
+      r= Review.create
+      expect(r.reviewed?).to be false
+    end
+  end
 
 
 end
--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -1,10 +1,10 @@
 module ControllerMacros
 
   def login_admin
-	  before(:each) do
-	    # TODO dont know what its doing
-	    @request.env["devise.mapping"] = Devise.mappings[:admin]
-	    sign_in FactoryGirl.create(:admin2) # Using factory girl as an example
-	  end
-	end
+    before(:each) do
+      # TODO dont know what its doing
+      @request.env["devise.mapping"] = Devise.mappings[:admin]
+      sign_in FactoryGirl.create(:admin2) # Using factory girl as an example
+    end
+  end
 end
